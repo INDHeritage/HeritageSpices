37a1cade650ab54dff5260667a02c6ea4f8e4454
INDHeritage <heritage.spices.pvtltd@gmail.com>
2026-07-21 03:48:10 +0530

    Fix: Change review modal trigger from anchor to button to prevent smooth scroll JS crash

--- commit.bat
+++ commit.bat
@@ -1,2 +1,2 @@
-git commit -a -m "UI: Make review link look like a clickable button"
+git commit -a -m "Fix: Change review modal trigger from anchor to button to prevent smooth scroll JS crash"
 git push origin main
